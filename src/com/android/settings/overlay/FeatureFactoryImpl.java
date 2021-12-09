4dfd60fe8ef7ba252ac2ec040f36eaac06cc6a50
Nauval Rizky <enuma.alrizky@gmail.com>
2021-12-09 11:38:52 +0700

    Settings: Add Game Space support
    Add support for Game Space via existing GameSettingsFeatureProvider

--- src/com/android/settings/overlay/FeatureFactoryImpl.java
+++ src/com/android/settings/overlay/FeatureFactoryImpl.java
@@ -33,6 +33,7 @@ import com.android.settings.applications.ApplicationFeatureProvider;
 import com.android.settings.applications.ApplicationFeatureProviderImpl;
 import com.android.settings.applications.GameSettingsFeatureProvider;
 import com.android.settings.applications.GameSettingsFeatureProviderImpl;
+import com.android.settings.applications.GameSpaceFeatureProvider;
 import com.android.settings.applications.appinfo.ExtraAppInfoFeatureProvider;
 import com.android.settings.applications.appinfo.ExtraAppInfoFeatureProviderImpl;
 import com.android.settings.aware.AwareFeatureProvider;
@@ -334,7 +335,7 @@ public class FeatureFactoryImpl extends FeatureFactory {
     @Override
     public GameSettingsFeatureProvider getGameSettingsFeatureProvider() {
         if (mGameSettingsFeatureProvider == null) {
-            mGameSettingsFeatureProvider = new GameSettingsFeatureProviderImpl();
+            mGameSettingsFeatureProvider = new GameSpaceFeatureProvider();
         }
         return mGameSettingsFeatureProvider;
     }
